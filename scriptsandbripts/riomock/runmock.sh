5067282b4afcb60f8fe87eba51353d59992172dd
sshiradwade <sshiradwade@synamedia.com>
2020-09-23 04:25:09 +0530

    Added condition

--- scriptsandbripts/riomock/runmock.sh
+++ scriptsandbripts/riomock/runmock.sh
@@ -3,7 +3,14 @@ mockstorageip=$(echo $mockstorageip | awk '{print $NF}')
 sed -i '/storage_endpoints=/c\storage_endpoints="'"$mockstorageip"'"' "$INI_PATH"
 echo "$mockstorageip Added to : $INI_PATH"
 
+read -p "Are you sure? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    # do dangerous stuff
+
 # RUN rio-bundle
 (cd $RIO_PATH; docker-compose run rio-bundle ./deploy.sh inventories/rio.ini)
 
+fi
 #(cd $INI_APTH; cat rio.ini)
